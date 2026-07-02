3e4972e4c819207cacde80237954434e4711de25
denysspitsevskyi-png <denys.spitsevskyi@gmail.com>
2026-07-02 13:11:41 +0200

    verbesserte Disign und Bot wie Gegner

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
@@ -10,7 +11,19 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 def index():
     # Сервер будет искать этот файл в папке templates
     return render_template('index.html')
-player_board =[
+my_board=[
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,1,1,1,1,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,1,1,1,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,1,1,0,0,0,0,1,1,0],
+    [0,0,0,0,0,0,0,0,0,0]
+]
+enemy_board =[
     [0,0,0,0,0,0,0,0,0,0],
     [0,1,1,1,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
@@ -22,22 +35,42 @@ player_board =[
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0]
 ]
-
+# Отправляем клиенту расстановку его собственных кораблей при запросе
+@socketio.on("request_my_ships")
+def handle_my_ships():
+    print("Клиенте запросил координаты своих кораблей")
+    emit("my_ships_response", {"board": my_board})
 # Слушаем тестовое событие выстрела от клиента
 @socketio.on('fire_shot')
 def handle_fire_shot(data):
     x = data.get('x')
     y = data.get('y')
     print(f"Сервер получил координаты выстрела: X={data['x']}, Y={data['y']}")
-    current_value = player_board[y][x]
+    current_value = enemy_board[y][x]
     if current_value == 2 or current_value == 3:
         status = "already_shot"
     elif current_value == 1:
         status ="hit"
-        player_board[y][x] = 2
+        enemy_board[y][x] = 2
     else:
         status = "miss"
-        player_board[y][x] = 3
+        enemy_board[y][x] = 3
+    # Если игрок промахнулся,запускаем ответный выстрел бота
+    if status=="miss":
+        # Бот выбирает случайную клетку на твоем поле
+        bot_x = random.randint(0,9)
+        bot_y = random.randint(0,9)
+        # Проверяем что там лежит
+        target_value = my_board[bot_y][bot_x]
+        if target_value == 1:
+            bot_status = "hit"
+            my_board[bot_y][bot_x] = 2 # Пометили что наш корабль подбит
+        else:
+            bot_status = "miss"
+            my_board[bot_y][bot_x] = 3 # Пометили промах бота
+        # Отправляем клиенту специальное событие "bot_shot"
+        emit("bot_shot",{"status": bot_status,"x":bot_x,"y":bot_y})
+
     # Отправляем ответ обратно клиенту
     emit('shot_response', {'status': status, 'x': x, 'y': y})
 
